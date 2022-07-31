1ba85ba89a48eb9a26d13ffec41a12ba3af6ffc8
Мар'яна <mariana@MacBook-Air-Marana.local>
2022-07-31 13:19:40 +0300

    Added categories

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -7,13 +7,13 @@
     navBody.classList.toggle("active");
   });
 
-  const navLinks = document.querySelector(".navigation-pages__link");
+  const navLinks = document.querySelectorAll(".navigation-pages__link");
   navLinks.forEach((navLink) => {
-    navLink.addEventListener("click", (_) => {
+    navLink.addEventListener("click", () => {
       if (iconBurger.classList.contains("active")) {
         document.body.classList.remove("lock");
-        navBody.classList.remove("active");
         iconBurger.classList.remove("active");
+        navBody.classList.remove("active");
       }
     });
   });
---
+++ js/categories-tab.js
@@ -0,0 +1,26 @@
+(function () {
+  let tabNav = document.querySelectorAll(".tab-item");
+  let tabContent = document.querySelectorAll(".tab__content");
+  let tabName;
+
+  tabNav.forEach((item) => {
+    item.addEventListener("click", selectTabNav);
+  });
+
+  function selectTabNav() {
+    tabNav.forEach((item) => {
+      item.classList.remove("active");
+    });
+    this.classList.add("active");
+    tabName = this.getAttribute("data-tab-name");
+    selectTabContent(tabName);
+  }
+
+  function selectTabContent(tabName) {
+    tabContent.forEach((item) => {
+      item.classList.contains(tabName)
+        ? item.classList.add("active")
+        : item.classList.remove("active");
+    });
+  }
+})();
