33c5036e47bc25413e218087363bdda9f2d6c3bc
Мар'яна <mariana@MacBook-Air-Marana.local>
2022-08-08 15:04:08 +0300

    Removed console.log

--- js/product-list.js
+++ js/product-list.js
@@ -2,23 +2,22 @@
   let currentRate = 1;
   const response = await fetch("products.json");
   const products = await response.json();
-  renderProducts(products, currentRate);
 
-  function renderProducts(products, currentRate) {
+  let unlock = true;
+  const timeout = 800;
+  renderProducts(products);
+
+  function renderProducts(products) {
     const productContainer = document.querySelector(".grid");
     productContainer.innerHTML = "";
     for (const product of products) {
       productContainer.innerHTML += `
-            <div class="grid-item">
+                <div class="grid-item">
                     <div class="grid-item__image-container">
-                        <img src="${
-                          product.image
-                        }" class="grid-item__image" alt="${product.title}">
+                        <img src="${product.image}" class="grid-item__image" alt="${product.title}">
                     </div>
                     <h3 class="grid-item__title">${product.title}</h3>
-                    <p class="grid-item__price">${(
-                      product.price * currentRate
-                    ).toFixed(2)}</p>
+                    <p class="grid-item__price">${product.price}</p>
                     <div class="grid-item__button-container">
                         <a href="#${product.info}" class="popup-link grid-item__button-info">Info</a>
                         <a href="" class="grid-item__button-buy">Buy →</a>
@@ -34,83 +33,85 @@
     );
     const ratesData = await response.json();
     currentRate = ratesData.rates[currency];
-    renderProducts(products, currentRate);
+    const productPrice = document.querySelectorAll(".grid-item__price");
+    for (let price of productPrice) {
+      price.innerHTML = (price.innerText * currentRate).toFixed(2);
+    }
   }
 
   document
     .querySelector(".convert-currency")
     .addEventListener("click", convertCurrency);
-  
-   const popupLinks = document.querySelectorAll('.popup-link');
-    const body = document.querySelector('body');
-    const popupCloseIcon = document.querySelectorAll('.close-popup');
 
-    let unlock = true;
-    const timeout = 800;
+  const popupLinks = document.querySelectorAll(".popup-link");
+  const body = document.querySelector("body");
+  const popupCloseIcon = document.querySelectorAll(".close-popup");
 
-    if (popupLinks.length > 0) {
-        for (let i = 0; i < popupLinks.length; i++){
-            const popupLink = popupLinks[i];
-            popupLink.addEventListener('click', function (e) {
-                const popupName = popupLink.getAttribute('href').replace('#', '');
-                const curentPopup = document.getElementById(popupName);
-                popupOpen(curentPopup);
-                e.preventDefault();
-            });
-        };
-    };
+  if (popupLinks.length > 0) {
+    for (let i = 0; i < popupLinks.length; i++) {
+      const popupLink = popupLinks[i];
+      popupLink.addEventListener("click", function (e) {
+        const popupName = popupLink.getAttribute("href").replace("#", "");
+        const curentPopup = document.getElementById(popupName);
+        console.log("fs");
+        popupOpen(curentPopup);
+        e.preventDefault();
+      });
+    }
+  }
 
-    if (popupCloseIcon.length > 0) {
-        for (let i = 0; i < popupCloseIcon.length; i++){
-            const el = popupCloseIcon[i];
-            el.addEventListener('click', function (e) {
-                popupClose(el.closest('.popup'));
-                e.preventDefault();
-            });
-        };
-    };
+  if (popupCloseIcon.length > 0) {
+    for (let i = 0; i < popupCloseIcon.length; i++) {
+      const el = popupCloseIcon[i];
+      el.addEventListener("click", function (e) {
+        popupClose(el.closest(".popup"));
+        e.preventDefault();
+      });
+    }
+  }
 
-    function popupOpen(curentPopup) {
-        if (curentPopup && unlock) {
-            curentPopup.classList.add('open');
-            bodyLock();
-            curentPopup.addEventListener('click', function (e) {
-                if (!e.target.closest(".popup__wrapper")) {
-                  popupClose(e.target.closest(".popup"));
-                };
-            });
-        };
-    };
+  function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
+      curentPopup.classList.add("open");
+      bodyLock();
+      curentPopup.addEventListener("click", function (e) {
+        if (!e.target.closest(".popup__wrapper")) {
+          popupClose(e.target.closest(".popup"));
+        }
+      });
+    }
+  }
 
-    function popupClose(popupActive, doUnlock = true) {
-        if (unlock) {
-            popupActive.classList.remove('open');
-            if (doUnlock) {
-                bodyUnLock();
-            };
-        };
-    };
+  function popupClose(popupActive, doUnlock = true) {
+    if (unlock) {
+      popupActive.classList.remove("open");
+      if (doUnlock) {
+        bodyUnLock();
+      }
+    }
+  }
 
-    function bodyLock() {
-        const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + 'px';
-        body.style.paddingRight = lockPaddingValue;
-        body.classList.add('lock');
+  function bodyLock() {
+    const lockPaddingValue =
+      window.innerWidth - document.querySelector("body").offsetWidth + "px";
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add("lock");
 
-        unlock = false;
-        setTimeout(function () {
-            unlock = true;
-        }, timeout);
-    };
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout);
+  }
 
-    function bodyUnLock() {
-        setTimeout(function () {
-            body.style.paddingRight = '0px';
-            body.classList.remove("lock");
-        }, timeout);
+  function bodyUnLock() {
+    setTimeout(function () {
+      body.style.paddingRight = "0px";
+      body.classList.remove("lock");
+    }, timeout);
 
-        unlock = false;
-        setTimeout(function () {
-            unlock = true;
-        }, timeout);  
-    };
+    unlock = false;
+    setTimeout(function () {
+      unlock = true;
+    }, timeout);
+  }
 })();
